76262a2caa6f92697d99324bdb47b41d88d94cb8
1312071 <1312071@student.hcmus.edu.vn>
2016-10-02 20:21:55 +0700

    update 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002032037) do
+ActiveRecord::Schema.define(version: 20161002050500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
 
   create_table "comments", force: :cascade do |t|
     t.string "ctitle"
     t.text   "comment"
   end
+
 
   create_table "posts", force: :cascade do |t|
     t.string  "title"
