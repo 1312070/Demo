65b4bd532086686dd138aed1d2b6f038bd6e70cd
Nguyen Pham Hoang Diem <1312070@student.hcmus.edu.vn>
2016-10-02 21:24:04 +0700

    Sua lan 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 		return nil if @user.nil?
 		return @user if @user.password == pass
 	end
-<<<<<<< HEAD
+
 	email_format = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
 	#validation:
 	validates :username, :password, :email, :presence => true
@@ -15,9 +15,6 @@ class User < ApplicationRecord
 	validates :username, :uniqueness => {:message => "username already taken"}
 	validates :password, :length => {:minimum => 5}
 	validates :email, :uniqueness => {:message => "email already taken"}
-	
-=======
->>>>>>> 9a120180d3ac1f6960364cc0f6de617437c21261
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   resources :users
-<<<<<<< HEAD
   resources :sessions
-=======
-<<<<<<< HEAD
-  resources :sessions
-  root 'sessions#new'
-=======
-<<<<<<< HEAD
-=======
-  resources :sessions
->>>>>>> login
->>>>>>> 9a120180d3ac1f6960364cc0f6de617437c21261
->>>>>>> fea3672cdc8612697698034c3cf9f1696d0b1b26
 end
