54b8223ffd3b96508790e869b2883c95c826cf66
1312071 <1312071@student.hcmus.edu.vn>
2016-10-02 11:36:46 +0700

    validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+		#validation:
+	validates :title, :content, :presence => true
+	validates :title, :length => {:minimum => 2}
+	validates :title, :uniqueness => {:message => "already taken"}
 end
