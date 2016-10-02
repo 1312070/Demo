0977f418eb4549eea63f67dd7db9b77094777d63
Nguyen Pham Hoang Diem <1312070@student.hcmus.edu.vn>
2016-10-02 20:35:47 +0700

    Sua lan 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,5 @@ class Post < ApplicationRecord
 	validates :title, :content, :presence => true
 	validates :title, :length => {:minimum => 2}
 	validates :title, :uniqueness => {:message => "already taken"}
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> login
+
